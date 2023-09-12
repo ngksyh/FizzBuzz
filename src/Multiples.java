fb20408b81ecb36fbf3b93e63ca03143ea68ea35
Yuhan Su <songyokkan@gmail.com>
2023-09-12 05:46:25 -0400

    multiples

---
+++ src/Multiples.java
@@ -0,0 +1,17 @@
+public class Multiples {
+
+    public static void main(String[] args) {
+        int cou = 0;
+        for (int i = 1; i < 1000; i ++){
+            if (i % 3 == 0 || i % 5 == 0){
+                cou ++;
+            }
+        }
+        System.out.println(cou);
+
+    }
+
+
+
+
+}
